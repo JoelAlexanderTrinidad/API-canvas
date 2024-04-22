70cf16c499b9f41c1f452158cd7533526c15c76a
JoelAlexanderTrinidad <joelalexandertrinidad@gmail.com>
2024-04-22 15:12:04 -0300

    scale images

--- src/main.js
+++ src/main.js
@@ -2,11 +2,15 @@ const canvas = document.querySelector("#board canvas");
 const ctx = canvas.getContext("2d");
 const { width: w, height: h } = canvas;
 
-ctx.font = "20pt courier"
+const img = new Image();
+img.src = "../res/images/snowflake.png";
+img.addEventListener("load", draw, false);
 
-const center = w / 2;
-ctx.textAlign = "center";
-for (let i = 0; i < 11; i++) {
-ctx.fillText("if you're in the game", center, i * 40); // i * 40 es la coordenada y en cada pasada lo va multiplicando por 40
+function draw(){
+    for (let i = 0; i < 100; i++) {
+        const x = Math.random() * w;
+        const y = Math.random() * h;
+        const scale = Math.random();
+        ctx.drawImage(img, x, y, img.width * scale, img.height * scale); 
+    }
 }
-ctx.strokeText("strokes the word", center, h - 30);
